699dea30e4501737a665319d8b1c271477bbf214
Rajajr <rajjr011@gmail.com>
2022-09-16 10:50:28 +0700

    UC-1 Player Start Postion 0

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderSimulator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC-1 Snake and Ladder game played with single player at start position 0
+            Console.WriteLine("Welcome to Snake And Ladder Game");
+            var Player =0;
+            Console.WriteLine("Player Starting Position:"+Player);
+            Console.ReadLine();
+        }
+    }
+}
