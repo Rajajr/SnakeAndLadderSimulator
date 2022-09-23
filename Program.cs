a7804772c3d52c717abe71e6623c92364f99ab7d
Rajajr <rajjr011@gmail.com>
2022-09-24 04:53:44 +0700

    The Player Checks For Options

--- Program.cs
+++ Program.cs
@@ -10,18 +10,45 @@ namespace SnakeAndLadderSimulator
     {
         static void Main(string[] args)
         {
-            //UC-2 Player rolls the die to get a number between 1 to 6
+            //UC-3 The Player Checks For a Option Whether Ladder,Snake or No Play
             Console.WriteLine("Welcome to Snake And Ladder Game");
-            Console.WriteLine("Player");
+            Console.WriteLine("Player1");
             //variables
-            var Player =0;
+            var Player1 =0;
             var Die = 0;
+            var Option = 0;
+
+            //constant
+            const int Ladder = 1;
+            const int Snake = 2;
+            const int No_Play = 0;
+
             //randomcheck 
             Random random = new Random();
             //range from 1 to 6
             Die=random.Next(1,7);
-            Console.WriteLine("Roll the die");
-            Console.WriteLine("Number:{0}",Die);
+            Console.WriteLine("Die Number:{0}",Die);
+
+            Option=random.Next(0,3);
+            Console.WriteLine("Player1 Option: {0}",Option);
+
+            switch (Option)
+            {
+                case Ladder:
+                    Console.WriteLine("Ladder");
+                    Console.WriteLine("Position: {0} ",Player1);
+                break;
+
+                case Snake:
+                    Console.WriteLine("Snake");
+                    Console.WriteLine("position: {0}",Player1);
+                    break;
+                case No_Play:
+                    Console.WriteLine("No Play");
+                    Console.WriteLine("position: {0}", Player1);
+                    break;   
+            }
+
             Console.ReadLine();
         }
     }
